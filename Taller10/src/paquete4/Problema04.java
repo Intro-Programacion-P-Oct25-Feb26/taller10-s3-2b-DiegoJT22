4361f7367230b0fdf1de947ad0842364a764ac67
DiegoJT22 <daigoestrlar22@gmail.com>
2025-12-18 12:49:24 -0500

    Resolución Parcial del Taller

--- Taller10/src/paquete4/Problema04.java
+++ Taller10/src/paquete4/Problema04.java
@@ -5,10 +5,49 @@
  */
 package paquete4;
 
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
  */
 public class Problema04 {
-    
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        int valorIngresado;
+        int[][] matrizA = new int[3][2];
+        int[][] matrizB = new int[3][2];
+        for (int i = 0; i < matrizA.length; i++) {
+            for (int j = 0; j < matrizA[i].length; j++) {
+                System.out.printf("Ingrese valor para la posición de la primera matriz [%d][%d]\t:",
+                        i, j);
+                valorIngresado = entrada.nextInt();
+                matrizA[i][j] = valorIngresado;
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < matrizB.length; i++) {
+            for (int j = 0; j < matrizB[i].length; j++) {
+                System.out.printf("Ingrese valor para la posición de la segunda matriz [%d][%d]\t:",
+                        i, j);
+                valorIngresado = entrada.nextInt();
+                matrizA[i][j] = valorIngresado;
+            }
+            System.out.println();
+        }
+        for (int fila = 0; fila < matrizA.length; fila++) {
+            for (int col = 0; col < matrizA[fila].length; col++) {
+                System.out.printf("%d\t", matrizA[fila][col]);
+            }
+            System.out.println("");
+        }
+        for (int fila = 0; fila < matrizB.length; fila++) {
+            for (int col = 0; col < matrizB[fila].length; col++) {
+                System.out.printf("%d\t", matrizB[fila][col]);
+            }
+            System.out.println("");
+        }
+    }
 }
