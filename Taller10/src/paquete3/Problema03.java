13d2249c83da46698d8e8a775b4bb0448fed0c19
DiegoJT22 <daigoestrlar22@gmail.com>
2025-12-18 12:12:23 -0500

    Solución Problema 03

--- Taller10/src/paquete3/Problema03.java
+++ Taller10/src/paquete3/Problema03.java
@@ -10,8 +10,39 @@ package paquete3;
  * @author reroes
  */
 public class Problema03 {
+
     public static void main(String[] args) {
-        
+        int contadorfallo;
+        int contador = 0;
+        String reporte = "";
+        int zonasSinFallos = 0;
+        boolean[][] sensores = {
+            {true, true, true, true, true, true},
+            {true, false, true, false, true, false},
+            {false, false, false, false, false, false},
+            {true, true, false, true, true, false}};
+
+        for (int i = 0; i < sensores.length; i++) {
+            contadorfallo = 0;
+            for (int j = 0; j < sensores[i].length; j++) {
+                if (sensores[i][j] == false) {
+                    contadorfallo = contadorfallo + 1;
+                } else {
+                    contador = contador + 1;
+                }
+            }
+            if (contador == sensores[i].length) {
+                zonasSinFallos = zonasSinFallos + 1;
+            } else {
+                System.out.printf(
+                        "La zona %d presenta al menos un fallo\n",
+                        i + 1);
+            }
+        }
+
+        System.out.printf(
+                "Zonas que funcionan sin fallos: %d\n",
+                zonasSinFallos);
+
     }
-    
 }
