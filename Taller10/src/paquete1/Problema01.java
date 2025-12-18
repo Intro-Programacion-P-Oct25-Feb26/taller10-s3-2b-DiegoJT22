098cc9cf360a8bf62ccbc8b8b4d3617a7f1e5b8e
DiegoJT22 <daigoestrlar22@gmail.com>
2025-12-18 11:26:32 -0500

    Solución Problema 01 - versión 2

--- Taller10/src/paquete1/Problema01.java
+++ Taller10/src/paquete1/Problema01.java
@@ -14,8 +14,10 @@ public class Problema01 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        int[] asistenciac = new int [4];
+
+        int[] asistenciac = new int[4];
+        int contador;
+        String reporte = "";
 
         boolean[][] asistencia = {
             {true, true, false, true, true},
@@ -25,13 +27,27 @@ public class Problema01 {
         };
 
         String[] estudiantes = {"Ana", "Luis", "Carlos", "María"};
-        
-        for (int i = 0;i < asistencia.length; i++){
-            
-            for(int j = 0;j < asistencia.length; i++){
-            
+
+        for (int i = 0; i < asistencia.length; i++) {
+            contador = 0;
+            for (int j = 0; j < asistencia[i].length; j++) {
+                if (asistencia[i][j] == true) {
+                    contador = contador + 1;
+                }
             }
-                    }
+            asistenciac[i] = contador;
+
+            if (asistenciac[i] == 5) {
+                reporte = String.format("%s Estudiante %s asistio %d (Asitenia todos los dias)\n",
+                        reporte, estudiantes[i], asistenciac[i]);
+            } else {
+                reporte = String.format("%s Estudiante %s asistio %d dias\n",
+                        reporte, estudiantes[i], asistenciac[i]);
+
+            }
+
+        }
+        System.out.printf("%s", reporte);
     }
 
 }
